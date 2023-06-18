ed938a6000c4bde855acea7bc56b73cd8b2675bd
Vitalii <35376308+Snowrost@users.noreply.github.com>
2023-06-18 04:10:34 +0300

    Update DZ_6.sql

--- dz6/DZ_6.sql
+++ dz6/DZ_6.sql
@@ -6,7 +6,7 @@ UPDATE Product
 SET quantity = 25
 WHERE product_id IN (1, 5);
 
--- delete warehouse from product
+-- delete products that store in one of warehouse from product
 
 DELETE FROM Product
 WHERE warehouse_id = 2;
