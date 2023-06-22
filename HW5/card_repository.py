bb92437436b1e104fa0ee010b7ecea7639ba3bb7
Vitalii <35376308+Snowrost@users.noreply.github.com>
2023-06-22 18:12:17 +0300

    Add files via upload

--- HW5/card_repository.py
+++ HW5/card_repository.py
@@ -1,19 +1,27 @@
-from card2 import Card, RBAC
-from enum import Enum
-import sqlite3
+from card import Card, RBAC
+import psycopg2
+import os
 
+# Класс репозиторію
 
-
-# Класс репозоиторію
 
 class CardRepository:
-    def __init__(self, db_file):
-        self.db_file = db_file
-        self.conn = sqlite3.connect(db_file)
+    def __init__(self):
+        self.db_connection = self.connect()
         self.create_table()
 
+    def connect(self):
+        connection = psycopg2.connect(
+            host="127.0.0.1",
+            port=5432,
+            dbname="test27",
+            user="postgres",
+            password=os.environ.get("DB_PASSWORD", "d87nv2at"),
+        )
+        return connection
+
     def create_table(self):
-        query = '''
+        query = """
         CREATE TABLE IF NOT EXISTS cards (
             card_id TEXT PRIMARY KEY,
             card_number TEXT,
@@ -23,53 +31,67 @@ class CardRepository:
             owner_id TEXT,
             card_status TEXT
         )
-        '''
-        self.conn.execute(query)
-        self.conn.commit()
+        """
+        cursor = self.db_connection.cursor()
+        cursor.execute(query)
+        self.db_connection.commit()
 
     def save_card(self, card, role):
         rbac = RBAC()
-        if not rbac.has_permission(role, 'save_card'):
-            raise PermissionError('Insufficient privileges to save card.')
+        if not rbac.has_permission(role, "save_card"):
+            raise PermissionError("Insufficient privileges to save card.")
 
-        query = '''
+        query = """
         INSERT INTO cards (card_id, card_number, expiry_date, cvv_code, issue_date, owner_id, card_status)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-        '''
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
         values = (
             card.card_id,
             card.card_number,
             card.expiry_date,
             card.cvv_code,
-            card.issue_date.strftime('%m/%d/%y'),
+            card.issue_date.strftime("%m/%d/%y"),
             str(card.owner_id),
-            card.card_status.value
+            card.card_status.value,
         )
-        self.conn.execute(query, values)
-        self.conn.commit()
+        cursor = self.db_connection.cursor()
+        cursor.execute(query, values)
+        self.db_connection.commit()
 
     def get_card_by_id(self, card_id, role):
         rbac = RBAC()
-        if not rbac.has_permission(role, 'get_card_by_id'):
-            raise PermissionError('Insufficient privileges to get card by ID.')
+        if not rbac.has_permission(role, "get_card_by_id"):
+            raise PermissionError("Insufficient privileges to get card by ID.")
 
-        query = '''
-        SELECT * FROM cards WHERE card_id = ?
-        '''
-        cursor = self.conn.execute(query, (card_id,))
+        query = """
+        SELECT * FROM cards WHERE card_id = %s
+        """
+        cursor = self.db_connection.cursor()
+        cursor.execute(query, (card_id,))
         row = cursor.fetchone()
         if row is not None:
             return self.row_to_card(row)
         return None
 
     def row_to_card(self, row):
-        card_id, card_number, expiry_date, cvv_code, issue_date, owner_id, card_status = row
-        return Card(card_id, card_number, expiry_date, cvv_code, issue_date, owner_id, card_status)
-
+        (
+            card_id,
+            card_number,
+            expiry_date,
+            cvv_code,
+            issue_date,
+            owner_id,
+            card_status,
+        ) = row
+        return Card(
+            card_id,
+            card_number,
+            expiry_date,
+            cvv_code,
+            issue_date,
+            owner_id,
+            card_status,
+        )
 
     def close(self):
-        self.conn.close()
-
-
-
-
+        self.db_connection.close()
