c555bd4435aa1ed4cfae599e2b50c9e6ab563686
Vitalii <35376308+Snowrost@users.noreply.github.com>
2023-07-16 19:52:20 +0300

    HW 10 for django project
    Add Templates, views for templates and adiing model to django admin and test for it

--- django/djangohw/cards/urls.py
+++ django/djangohw/cards/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import CardView
+from .views import CardViewTemp, CardCreateTemp #,CardView
 
 urlpatterns = [
-    path('cards/', CardView.as_view(http_method_names=["get", "post"]), name='card-create-get'),
+    # path('card/', CardView.as_view(http_method_names=["get", "post"]), name='card-create-get'),
+    path('cards/', CardViewTemp.as_view(), name='card_list'),
+    path('cards/create/', CardCreateTemp.as_view(), name='create_card'),
+    path('cards/save/', CardCreateTemp.as_view(), name='save_card'),
 ]
