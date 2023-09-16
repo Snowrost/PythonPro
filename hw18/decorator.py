ec7b314a1073e3f2958b76587d4887888657a775
Vitalii <35376308+Snowrost@users.noreply.github.com>
2023-09-16 23:16:16 +0300

    Update decorator.py

--- hw18/decorator.py
+++ hw18/decorator.py
@@ -2,20 +2,21 @@ import time
 import asyncio
 from functools import wraps
 
+# Define the decorator
 def execution_time(func):
     @wraps(func)
     async def wrapper_async(*args, **kwargs):
         start_time = time.time()
         result = await func(*args, **kwargs)
         end_time = time.time()
-        print(f"Function '{func.__name__}' executed in {end_time - start_time:.6f} seconds (async)")
+        print(f"Function executed in {end_time - start_time:.6f} seconds (async)")
         return result
 
     def wrapper_sync(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Function '{func.__name__}' executed in {end_time - start_time:.6f} seconds (sync)")
+        print(f"Function executed in {end_time - start_time:.6f} seconds (sync)")
         return result
 
     if asyncio.iscoroutinefunction(func):
